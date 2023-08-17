cb1d2adfb71e71aa93f1618aec0f11c19e8aca84
Mohammad <moh.hossain98@gmail.com>
2023-08-17 15:39:13 -0400

    grid-and-flex

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="grid-container">
+        <nav className="grid-item header">
+          <a>Home</a>
+          <a>About</a>
+          <a>Posts</a>
+        </nav>
+
+        <div className="grid-item content">
+          <h1>This is the title</h1>
+          <p>
+            Sweet beast. Asdflkjaertvlkjasntvkjn (sits on keyboard). Scratch me
+            now! stop scratching me! trip owner up in kitchen i want food. Catch
+            eat throw up catch eat throw up bad birds purrrrrr murr i hate
+            humans they are so annoying and i hate cucumber pls dont throw it at
+            me hiss and stare at nothing then run suddenly away or pooping
+            rainbow while flying in a toasted bread costume in space meow meow,
+            i tell my human.{" "}
+          </p>
+        </div>
+        <div className="grid-item content">
+          <h1>This is the title</h1>
+          <p>
+            Sweet beast. Asdflkjaertvlkjasntvkjn (sits on keyboard). Scratch me
+            now! stop scratching me! trip owner up in kitchen i want food. Catch
+            eat throw up catch eat throw up bad birds purrrrrr murr i hate
+            humans they are so annoying and i hate cucumber pls dont throw it at
+            me hiss and stare at nothing then run suddenly away or pooping
+            rainbow while flying in a toasted bread costume in space meow meow,
+            i tell my human.{" "}
+          </p>
+        </div>
+        <div className="grid-item content">
+          <h1>This is the title</h1>
+          <p>
+            Sweet beast. Asdflkjaertvlkjasntvkjn (sits on keyboard). Scratch me
+            now! stop scratching me! trip owner up in kitchen i want food. Catch
+            eat throw up catch eat throw up bad birds purrrrrr murr i hate
+            humans they are so annoying and i hate cucumber pls dont throw it at
+            me hiss and stare at nothing then run suddenly away or pooping
+            rainbow while flying in a toasted bread costume in space meow meow,
+            i tell my human.{" "}
+          </p>
+        </div>
+        <div className="grid-item content">
+          <h1>This is the title</h1>
+          <p>
+            Sweet beast. Asdflkjaertvlkjasntvkjn (sits on keyboard). Scratch me
+            now! stop scratching me! trip owner up in kitchen i want food. Catch
+            eat throw up catch eat throw up bad birds purrrrrr murr i hate
+            humans they are so annoying and i hate cucumber pls dont throw it at
+            me hiss and stare at nothing then run suddenly away or pooping
+            rainbow while flying in a toasted bread costume in space meow meow,
+            i tell my human.{" "}
+          </p>
+        </div>
+        {/* <div className="grid-item content">
+          <h1>This is the title</h1>
+          <p>
+            Sweet beast. Asdflkjaertvlkjasntvkjn (sits on keyboard). Scratch me
+            now! stop scratching me! trip owner up in kitchen i want food. Catch
+            eat throw up catch eat throw up bad birds purrrrrr murr i hate
+            humans they are so annoying and i hate cucumber pls dont throw it at
+            me hiss and stare at nothing then run suddenly away or pooping
+            rainbow while flying in a toasted bread costume in space meow meow,
+            i tell my human.{" "}
+          </p>
+        </div> */}
+        <div className="grid-item content content-6">
+          <h1>This is the 6th title</h1>
+          <p>
+            Sweet beast. Asdflkjaertvlkjasntvkjn (sits on keyboard). Scratch me
+            now! stop scratching me! trip owner up in kitchen i want food. Catch
+            eat throw up catch eat throw up bad birds purrrrrr murr i hate
+            humans they are so annoying and i hate cucumber pls dont throw it at
+            me hiss and stare at nothing then run suddenly away or pooping
+            rainbow while flying in a toasted bread costume in space meow meow,
+            i tell my human.{" "}
+          </p>
+        </div>
+
+        <div className="grid-item footer">footer</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
